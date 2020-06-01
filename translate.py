a6135158efb7676429a1bf5298def3efdbbcf404
anonimQwerty <61067633+anonimQwerty@users.noreply.github.com>
2020-06-01 14:37:02 +0300

    Add files via upload

---
+++ translate.py
@@ -0,0 +1,25 @@
+official={
+	'Input_string':'Айпи шизоида',
+	'launage_string':'ru', 
+	'First':'Шизоид использовал торент первый раз: ',
+	'Last':'шизоид использовал торент последний раз: ',
+	'Category':'тип торента: ',
+	'Name':'название торента: ',
+	'Size':'размер торента: '
+}
+
+
+
+
+durka={
+	'Input_string':'TorrentSeeker@target_ip',
+	'launage_string':'en', # Язык сайта
+	'First':'First seen: ',
+	'Last':'last seen: ',
+	'Category':'category: ',
+	'Name':'name: ',
+	'Size':'size: '
+}
+
+
+# Для создания своего языкового пакета нужно создать переменную типа словарь как на примере выше и импортировать ёё в основной скрипт типа from translate import переменная
