9a040073b79ef3c2b1b4a9a73bdf5f99cd9850ac
anonimQwerty <61067633+anonimQwerty@users.noreply.github.com>
2020-07-10 22:12:40 +0300

    Update translate.py
    added russian launage pack

--- translate.py
+++ translate.py
@@ -1,24 +1,35 @@
-official={
+durka={
 	'Input_string':'Айпи шизоида',
 	'launage_string':'ru', 
 	'First':'Шизоид использовал торент первый раз: ',
 	'Last':'шизоид использовал торент последний раз: ',
 	'Category':'тип торента: ',
 	'Name':'название торента: ',
-	'Size':'размер торента: '
+	'Size':'размер торента: ',
+	'Savechoice':'Сохранять результат поиска даного шизика? y/n'
 }
 
 
+russian={
+	'Input_string':'TorrentSeeker@target_ip',
+	'launage_string':'ru', # Язык сайта
+	'First':'Использовано первый раз: ',
+	'Last':'использовано последний раз: ',
+	'Category':'категгория: ',
+	'Name':'имя: ',
+	'Size':'размер торента: ',
+	'Savechoice':'Сохранять результат? y/n '
+}
 
-
-durka={
+official={
 	'Input_string':'TorrentSeeker@target_ip',
 	'launage_string':'en', # Язык сайта
 	'First':'First seen: ',
 	'Last':'last seen: ',
 	'Category':'category: ',
 	'Name':'name: ',
-	'Size':'size: '
+	'Size':'size: ',
+	'Savechoice':'Save result? y/n '
 }
 
 
